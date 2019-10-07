30da69f89d9c1064ce80ad27c0b194146726c82f
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-10-07 20:40:30 +0200

    Add magnitude from audio in bands

--- audio_preprocessor/main.py
+++ audio_preprocessor/main.py
@@ -2,22 +2,31 @@ import math
 import numpy as np
 import sounddevice as sd
 
-block_duration = 50
 device = None
-samplerate = sd.query_devices(device, 'input')['default_samplerate']
+samplerate = sd.query_devices(device, 'input')['default_samplerate']  # sample rate per second
+block_duration = 100 # blocksize in milliseconds
+block_size = int(samplerate * block_duration / 1000)
+low, high = [100, 2000]
+freqbands_n = 12
+delta_f = (high - low) / (freqbands_n - 1)
+fftsize = math.ceil(samplerate / delta_f)
+low_bin = math.floor(low / delta_f)
 
-def callback(indata, frames, time, status):
+def callback(inputdata, frames, time, status):
     if status:
         print(status)
-    if any(indata):
-        # magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
-        print("some audio input 😎")
+    if any(inputdata):
+        magnitude = np.abs(np.fft.rfft(inputdata[:, 0], n=fftsize))
+        magnitude *= 10 / fftsize  # normalize
+        intensity_band = [np.clip(x, 0, 1) for x in magnitude[low_bin:low_bin + freqbands_n]]
+        print(intensity_band)
+
     else:
-        print('no input')
+        print('no audio input fetched')
 
 
 with sd.InputStream(device=device, channels=1, callback=callback,
-                    blocksize=int(samplerate * block_duration / 1000),
+                    blocksize=block_size,
                     samplerate=samplerate):
     while True:
         response = input()
