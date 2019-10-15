279bb9b8e3a3bd50d1c516027da2820b97d5db8d
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-10-15 10:16:48 +0200

    Refactor for larger freq bands

--- shared/cppn_renderer.py
+++ shared/cppn_renderer.py
@@ -29,24 +29,18 @@ class CppnRenderer(object):
         f0_px_list = [[current_itensity_band[0] * tune for j in range(x_dim)] for i in range(x_dim)]
         f1_px_list = [[current_itensity_band[1] * tune for j in range(x_dim)] for i in range(x_dim)]
 
-        r_tensor = cppn(x_in=torch.tensor(x_px_list, dtype=torch.float32),
-                        y_in=torch.tensor(y_px_list, dtype=torch.float32),
-                        rgb_in=torch.tensor(r_px_list, dtype=torch.float32),
-                        f_0=torch.tensor(f0_px_list, dtype=torch.float32),
-                        f_1=torch.tensor(f1_px_list, dtype=torch.float32),
-                        )
-        g_tensor = cppn(x_in=torch.tensor(x_px_list, dtype=torch.float32),
-                        y_in=torch.tensor(y_px_list, dtype=torch.float32),
-                        rgb_in=torch.tensor(g_px_list, dtype=torch.float32),
-                        f_0=torch.tensor(f0_px_list, dtype=torch.float32),
-                        f_1=torch.tensor(f1_px_list, dtype=torch.float32),
-                        )
-        b_tensor = cppn(x_in=torch.tensor(x_px_list, dtype=torch.float32),
-                        y_in=torch.tensor(y_px_list, dtype=torch.float32),
-                        rgb_in=torch.tensor(b_px_list, dtype=torch.float32),
-                        f_0=torch.tensor(f0_px_list, dtype=torch.float32),
-                        f_1=torch.tensor(f1_px_list, dtype=torch.float32),
-                        )
+        f_dict = {"x_in": torch.tensor(x_px_list, dtype=torch.float32),
+                  "y_in": torch.tensor(y_px_list, dtype=torch.float32),
+                  "f_0": torch.tensor(f0_px_list, dtype=torch.float32),
+                  "f_1": torch.tensor(f1_px_list, dtype=torch.float32),
+                  "rgb_in": torch.tensor(r_px_list, dtype=torch.float32)}
+
+        r_tensor = cppn(**f_dict)
+        f_dict["rgb_in"] = torch.tensor(g_px_list, dtype=torch.float32)
+        g_tensor = cppn(**f_dict)
+        f_dict["rgb_in"] = torch.tensor(b_px_list, dtype=torch.float32)
+        b_tensor = cppn(**f_dict)
+
         rgb_tensor = torch.stack([r_tensor, g_tensor, b_tensor], 0)
 
         pil_img = torchvision.transforms.ToPILImage()(rgb_tensor)
