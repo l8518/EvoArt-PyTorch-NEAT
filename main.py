23c2667efa68a4edea0ebf3db8a4a9649a03b665
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-06 00:26:16 +0100

    Add "dancing" individuals + Tornado Refactor (WiP)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import evoart_server.flask
+import tornado
+from tornado.ioloop import IOLoop
+
+import evoart_server.app
 
 def run_evoart_server():
-    flask_evo = evoart_server.flask.get_instance()
-    return flask_evo.run(threaded=True)
+    app_evo = evoart_server.app.get_instance()
+
+
+    server = tornado.httpserver.HTTPServer(app_evo)
+    server.bind(8888)
+    server.start()
+    IOLoop.current().start()
+
+    return
 
 if __name__ == "__main__":
     run_evoart_server()
